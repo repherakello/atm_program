74c5b6ccfd582115a8955ecf83550232e0c0a9b6
Repher Akello <akellorepher@gmail.com>
2024-04-16 11:41:58 +0300

    updating the latest

--- Introduction/hello.py
+++ Introduction/hello.py
@@ -6,10 +6,22 @@ def MY_NAME():
     """
     this function takes the name of the user and display on the console
     """
+    
+   
  
     global x
     name = "akello"
     name2 = "repher"
-    print("My name is",name,name2)
+    for count in range(3):
+        if name == 'akello' and name2 == 'repher':
+            print("My name is",name,name2)
+            break
+        elif name != 'akello' and name2 == 'repher':
+            print('input',name,'wrong but',name2,'correct')
+            return count
+
+        else:
+             print('wrong input:') 
+             break
+           
 MY_NAME() 
-     
